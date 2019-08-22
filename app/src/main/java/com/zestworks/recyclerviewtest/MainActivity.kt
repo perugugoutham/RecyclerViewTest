89255d3c99d64386df56a6b79418de1513fc080b
perugu.goutham <perugu.goutham@zohocorp.com>
2019-08-22 21:15:42 +0530

    Image rendering on scroll state ideal

--- app/src/main/java/com/zestworks/recyclerviewtest/MainActivity.kt
+++ app/src/main/java/com/zestworks/recyclerviewtest/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,8 +22,19 @@ class MainActivity : AppCompatActivity() {
             mutableListOf.add("item $i")
         }
         recyclerView.layoutManager = LinearLayoutManager(applicationContext)
-        recyclerView.adapter = TestAdapter(mutableListOf)
-        recyclerView.smoothScrollToPosition(2000)
+        val testAdapter = TestAdapter(mutableListOf)
+        recyclerView.adapter = testAdapter
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE){
+                    val linearLayoutManager = recyclerView.layoutManager as LinearLayoutManager
+                    val from = linearLayoutManager.findFirstVisibleItemPosition()
+                    val to = linearLayoutManager.findLastVisibleItemPosition()
+                    testAdapter.scrollStateIsIdeal(from, to)
+                }
+            }
+        })
     }
 }
 
@@ -35,9 +47,27 @@ class TestAdapter(val items: List<String>): RecyclerView.Adapter<TestViewHolder>
     override fun onBindViewHolder(holder: TestViewHolder, position: Int) {
         Log.d("recyclerView", "position : $position, holder = $holder")
         holder.textView.text = items[position]
+        holder.itemView.findViewById<ImageView>(R.id.image_view).setImageBitmap(null)
+    }
+
+    override fun onBindViewHolder(holder: TestViewHolder, position: Int, payloads: MutableList<Any>) {
+        super.onBindViewHolder(holder, position, payloads)
+        payloads.forEach {
+            if (it is ImagePayLoad){
+                //This place is where we shall start the bitmap request
+                holder.itemView.findViewById<ImageView>(R.id.image_view).setImageResource(R.mipmap.ic_launcher_round)
+            }
+        }
+    }
+
+    fun scrollStateIsIdeal(from: Int, to: Int) {
+        notifyItemRangeChanged(from, (to - from) + 1, ImagePayLoad)
     }
 }
 
 class TestViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     val textView: TextView = itemView.findViewById(R.id.textView)
 }
+
+sealed class PayLoad
+object ImagePayLoad: PayLoad()
